83af82edd91574007f8b666202845b14c9e9866c
enrui-shi <enruishi98@gmail.com>
2020-12-15 14:24:12 -0500

    2

---
+++ test.py
@@ -0,0 +1,80 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import glob
+import os
+from PIL import Image
+import rawpy
+
+import model
+import unit
+
+
+input_dir = './data/Sony/short/'
+gt_dir = './data/Sony/long/'
+
+result_dir = './result/3/'
+PATH = './3/weights_1999.pth'
+# get test IDs
+test_fns = glob.glob(gt_dir + '/1*.ARW')
+test_ids = [int(os.path.basename(test_fn)[0:5]) for test_fn in test_fns]
+
+
+
+model = model.Unet(4)
+model.load_state_dict(torch.load(PATH))
+model.eval()
+
+
+for test_id in test_ids:
+    # test the first image in each sequence
+    in_files = glob.glob(input_dir + '%05d_00*.ARW' % test_id)
+    print(in_files)
+    for k in range(len(in_files)):
+        in_path = in_files[k]
+        in_fn = os.path.basename(in_path)
+        gt_files = glob.glob(gt_dir + '%05d_00*.ARW' % test_id)
+        gt_path = gt_files[0]
+        gt_fn = os.path.basename(gt_path)
+        in_exposure = float(in_fn[9:-5])
+        gt_exposure = float(gt_fn[9:-5])
+        ratio = min(gt_exposure / in_exposure, 300)
+
+        raw = rawpy.imread(in_path)
+        input_full = np.expand_dims(unit.pack_raw(raw), axis=0) * ratio
+
+        im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        # scale_full = np.expand_dims(np.float32(im/65535.0),axis = 0)*ratio
+        scale_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+        gt_raw = rawpy.imread(gt_path)
+        im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        gt_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+        input_full = torch.from_numpy(np.minimum(input_full, 1.0))
+
+        input_full = input_full.permute(0,3,1,2)
+
+
+        output = model(input_full)
+        output = output.permute(0,2,3,1)
+        output = output.detach().numpy()
+        output = np.minimum(np.maximum(output, 0), 1)
+        
+
+        output = output[0, :, :, :]
+        gt_full = gt_full[0, :, :, :]
+        scale_full = scale_full[0, :, :, :]
+        scale_full = scale_full * np.mean(gt_full) / np.mean(scale_full)
+
+
+        Image.fromarray((output*255).astype(np.uint8), mode = 'RGB').save(
+            result_dir + 'final/%5d_00_%d_out.png' % (test_id, ratio))
+        Image.fromarray((scale_full * 255).astype(np.uint8), mode = 'RGB').save(
+            result_dir + 'final/%5d_00_%d_scale.png' % (test_id, ratio))
+        Image.fromarray((gt_full * 255).astype(np.uint8), mode = 'RGB').save(
+            result_dir + 'final/%5d_00_%d_gt.png' % (test_id, ratio))
+        # scipy.misc.toimage(output * 255, high=255, low=0, cmin=0, cmax=255).save(
+        #     result_dir + 'final/%5d_00_%d_out.png' % (test_id, ratio))
+        # scipy.misc.toimage(scale_full * 255, high=255, low=0, cmin=0, cmax=255).save(
+        #     result_dir + 'final/%5d_00_%d_scale.png' % (test_id, ratio))
+        # scipy.misc.toimage(gt_full * 255, high=255, low=0, cmin=0, cmax=255).save(
+        #     result_dir + 'final/%5d_00_%d_gt.png' % (test_id, ratio))
--- train.py
+++ train.py
@@ -110,51 +110,6 @@ def loaddata(ind,ps):
     return input_patch, gt_patch
 
 
-# ps = 512
-# for ind in np.random.permutation(len(train_ids)):
-#     train_id = train_ids[ind]
-#     in_files = glob.glob(input_dir + '%05d_00*.ARW' % train_id)
-#     in_path = in_files[np.random.randint(0, len(in_files))]
-#     in_fn = os.path.basename(in_path)
-#     gt_files = glob.glob(gt_dir + '%05d_00*.ARW' % train_id)
-#     gt_path = gt_files[0]
-#     gt_fn = os.path.basename(gt_path)
-#     in_exposure = float(in_fn[9:-5])
-#     gt_exposure = float(gt_fn[9:-5])
-#     ratio = min(gt_exposure / in_exposure, 300)
-#     if input_images[str(ratio)[0:3]][ind] is None:
-#         raw = rawpy.imread(in_path)
-#         out = torch.from_numpy(np.expand_dims(pack_raw(raw), axis=0) * ratio)
-#         input_images[str(ratio)[0:3]][ind] = out
-#         gt_raw = rawpy.imread(gt_path)
-#         im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-#         gt_out = np.expand_dims(np.float32(im / 65535.0), axis=0)
-#         gt_images[ind] = torch.from_numpy(gt_out)
-#     else:
-#         print(1)
-
-#     # crop
-#     H = input_images[str(ratio)[0:3]][ind].shape[1]
-#     W = input_images[str(ratio)[0:3]][ind].shape[2]
-
-#     xx = np.random.randint(0, W - ps)
-#     yy = np.random.randint(0, H - ps)
-#     input_patch = input_images[str(ratio)[0:3]][ind][:, yy:yy + ps, xx:xx + ps, :]
-#     gt_patch = gt_images[ind][:, yy * 2:yy * 2 + ps * 2, xx * 2:xx * 2 + ps * 2, :]
-
-#     if np.random.randint(2, size=1)[0] == 1:  # random flip
-#         input_patch = torch.flip(input_patch, [1])
-#         gt_patch = torch.flip(gt_patch, [1])
-#     if np.random.randint(2, size=1)[0] == 1:
-#         input_patch = torch.flip(input_patch, [2])
-#         gt_patch = torch.flip(gt_patch, [2])
-#     if np.random.randint(2, size=1)[0] == 1:  # random transpose
-#         input_patch = input_patch.permute(0, 2, 1, 3)
-#         gt_patch = gt_patch.permute(0, 2, 1, 3)
-#     input_patch = input_patch.permute(0,3,1,2)
-#     gt_patch = gt_patch.permute(0,3,1,2)
-#     print(input_patch.shape)
-
 learning_rate = 0.0001
 model = model.Unet(4).to(device)
 model.train()
